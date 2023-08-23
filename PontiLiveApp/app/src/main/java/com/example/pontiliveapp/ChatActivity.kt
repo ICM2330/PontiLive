746c927da51077060e104df25e14b6e8a294da9f
Lukas Rodriguez <lukasrodriguez1997@gmail.com>
2023-08-22 22:46:57 -0500

    actividad del chat implementada

--- PontiLiveApp/app/src/main/java/com/example/pontiliveapp/ChatActivity.kt
+++ PontiLiveApp/app/src/main/java/com/example/pontiliveapp/ChatActivity.kt
@@ -1,25 +1,72 @@
 package com.example.pontiliveapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pontiliveapp.databinding.ActivityChatBinding
-import com.example.pontiliveapp.databinding.ActivityConfigBinding
+import com.example.pontiliveapp.databinding.ActivityMainBinding
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var messageEditText: EditText
+    private lateinit var chatContainer: LinearLayout
     private lateinit var binding: ActivityChatBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.profileButton.setOnClickListener{
-            startActivity(Intent(baseContext, ProfileActivity::class.java))
-        }
+        messageEditText = findViewById(R.id.messageEditText)
+        chatContainer = findViewById(R.id.chatContainer)
 
-        binding.mapButton.setOnClickListener{
+        setListeners();
+
+    }
+
+    fun setListeners(){
+        binding.botonBack.setOnClickListener{
             startActivity(Intent(baseContext, MapActivity::class.java))
+        }
+    }
+
+    fun sendMessage(view: View) {
+        val message = messageEditText.text.toString().trim()
+        if (message.isNotEmpty()) {
+            addMessageToChat(message)
+            messageEditText.text.clear()
+        }
+    }
+
+    private fun addMessageToChat(message: String) {
+        val messageTextView = TextView(this)
+        messageTextView.text = message
+
+        // Set text alignment to the right
+        messageTextView.textAlignment = View.TEXT_ALIGNMENT_TEXT_END
+
+        // Set layout parameters to align to the right and stack from bottom
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.gravity = Gravity.END
+
+        messageTextView.layoutParams = layoutParams
+
+        chatContainer.addView(messageTextView)
+
+        // Scroll the ScrollView to the bottom
+        val scrollView = findViewById<ScrollView>(R.id.scrollView)
+        scrollView.post {
+            scrollView.fullScroll(View.FOCUS_DOWN)
         }
     }
 }
