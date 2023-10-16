7cc199605bf41f57074a1329158499c53d5584fc
gams2004 <gabrielmartin2k4@gmail.com>
2023-10-16 04:22:24 -0500

    Added maps connection

--- PontiLiveApp/app/src/main/java/com/example/pontiliveapp/dialogs/ListDialogFragment.kt
+++ PontiLiveApp/app/src/main/java/com/example/pontiliveapp/dialogs/ListDialogFragment.kt
@@ -2,25 +2,22 @@ package com.example.pontiliveapp.dialogs
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import com.example.pontiliveapp.R
-import com.example.pontiliveapp.databinding.FragmentInfoDialogBinding
+import com.example.pontiliveapp.activities.MapActivity
 import com.example.pontiliveapp.databinding.FragmentListDialogBinding
 
-class ListDialogFragment(private val onSubmitClickListener: (Any?) -> Unit
-): DialogFragment() {
+class ListDialogFragment(): DialogFragment() {
 
     private lateinit var binding : FragmentListDialogBinding
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = FragmentListDialogBinding.inflate(LayoutInflater.from(context))
+        setListeners()
 
         val builder = AlertDialog.Builder(requireActivity())
         builder.setView(binding.root)
@@ -28,6 +25,39 @@ class ListDialogFragment(private val onSubmitClickListener: (Any?) -> Unit
         val dialog = builder.create()
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         return dialog
+
     }
 
+    fun setListeners(){
+        binding.imagen1.setOnClickListener {
+            val i = Intent(context, MapActivity::class.java)
+            i.putExtra("nombre","Donde Cris");
+            startActivity(Intent(i))
+        }
+        binding.imagen2.setOnClickListener {
+            val i = Intent(context, MapActivity::class.java)
+            i.putExtra("nombre","Biblioteca General");
+            startActivity(Intent(i))
+        }
+        binding.imagen3.setOnClickListener {
+            val i = Intent(context, MapActivity::class.java)
+            i.putExtra("nombre","Básicas");
+            startActivity(Intent(i))
+        }
+        binding.imagen4.setOnClickListener {
+            val i = Intent(context, MapActivity::class.java)
+            i.putExtra("nombre","Parque Nacional");
+            startActivity(Intent(i))
+        }
+        binding.imagen5.setOnClickListener {
+            val i = Intent(context, MapActivity::class.java)
+            i.putExtra("nombre","Canchas Sintéticas");
+            startActivity(Intent(i))
+        }
+        binding.imagen6.setOnClickListener {
+            val i = Intent(context, MapActivity::class.java)
+            i.putExtra("nombre","Edificio Giraldo");
+            startActivity(Intent(i))
+        }
+    }
 }
