1bd582389a68178eeaa31d04a1868443dba84f37
gams2004 <gabrielmartin2k4@gmail.com>
2023-10-16 04:24:26 -0500

    Added listeners with dialogs

--- PontiLiveApp/app/src/main/java/com/example/pontiliveapp/activities/ProfileActivity.kt
+++ PontiLiveApp/app/src/main/java/com/example/pontiliveapp/activities/ProfileActivity.kt
@@ -3,13 +3,14 @@ package com.example.pontiliveapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.pontiliveapp.databinding.ActivityProfileBinding
 import com.example.pontiliveapp.dialogs.InfoDialogFragment
 import com.example.pontiliveapp.dialogs.ListDialogFragment
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    val bundle = Bundle()
+    val fragB = InfoDialogFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +35,31 @@ class ProfileActivity : AppCompatActivity() {
         }
 
         binding.ing.setOnClickListener{
-            InfoDialogFragment { quantity ->
-                Toast.makeText(this, "Éxito", Toast.LENGTH_SHORT).show()
-            }.show(supportFragmentManager, "dialog")
+            bundle.putString("nombre", "Edificio de Ingeniería")
+            fragB.arguments = bundle
+            fragB.show(supportFragmentManager, "dialog")
+        }
+
+        binding.arca.setOnClickListener{
+            bundle.putString("nombre", "Arca")
+            fragB.arguments = bundle
+            fragB.show(supportFragmentManager, "dialog")
+        }
+
+        binding.arqui.setOnClickListener{
+            bundle.putString("nombre", "Arqui-diseño")
+            fragB.arguments = bundle
+            fragB.show(supportFragmentManager, "dialog")
+        }
+
+        binding.artes.setOnClickListener{
+            bundle.putString("nombre", "Artes")
+            fragB.arguments = bundle
+            fragB.show(supportFragmentManager, "dialog")
         }
 
         binding.morePlaces.setOnClickListener{
-            ListDialogFragment { quantity ->
-            Toast.makeText(this, "Éxito", Toast.LENGTH_SHORT).show()
-        }.show(supportFragmentManager, "dialog")
-
+            ListDialogFragment().show(supportFragmentManager, "dialog")
         }
     }
 }
