1b7dd51d2574eb07b5b58c15dbfb41bc87546035
gams2004 <gabrielmartin2k4@gmail.com>
2023-10-16 04:22:14 -0500

    Added maps connection

--- PontiLiveApp/app/src/main/java/com/example/pontiliveapp/dialogs/InfoDialogFragment.kt
+++ PontiLiveApp/app/src/main/java/com/example/pontiliveapp/dialogs/InfoDialogFragment.kt
@@ -2,15 +2,28 @@ package com.example.pontiliveapp.dialogs
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.fragment.app.DialogFragment
+import com.example.pontiliveapp.activities.MapActivity
 import com.example.pontiliveapp.databinding.FragmentInfoDialogBinding
+import com.example.pontiliveapp.model.getLugarName
 
-class InfoDialogFragment(private val onSubmitClickListener: (Any?) -> Unit
-): DialogFragment() {
+//Clase para desplegar información sobre los lugares
+//Para usarla se debe pasar por argumentos de la clase un bundle con el nombre del lugar a desplegar
+//Por ejemplo:
+
+/* *Dentro del Listener del elemento que despliege el díalogo (Botón, marcador...)*
+* val bundle = Bundle()
+* val fragB = InfoDialogFragment()
+* bundle.putString("nombre", "Nombre del edificio")
+* fragB.arguments = bundle
+* fragB.show(supportFragmentManager, "dialog")
+*/
+class InfoDialogFragment() : DialogFragment() {
 
     private lateinit var binding : FragmentInfoDialogBinding
 
@@ -20,11 +33,23 @@ class InfoDialogFragment(private val onSubmitClickListener: (Any?) -> Unit
         val builder = AlertDialog.Builder(requireActivity())
         builder.setView(binding.root)
 
+        val lugarB = arguments?.getString("nombre")?.let { getLugarName(it) }
 
-        binding.comenzarRuta.setOnClickListener {
-            //onSubmitClickListener.invoke(binding.etAmount.text.toString().toFloat())
+        binding.lugar.text = lugarB!!.nombre
+        binding.espacio.text = lugarB!!.espacio
+        binding.poblacion.text = lugarB!!.poblacionActual.toString()
+        binding.distancia.text = lugarB!!.distanciaActual.toString()
+        binding.imagen.setImageResource(lugarB.IdImagen)
+
+
+
+        binding.comenzarRuta.setOnClickListener{
+            val i = Intent(context, MapActivity::class.java)
+            i.putExtra("nombre",lugarB!!.nombre);
+            startActivity(Intent(i))
             dismiss()
         }
+
 
         val dialog = builder.create()
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
