c57f5f77b0a78f931a0d04af21ca2d4560958ba5
Juan Lesmes <juanfernandolesmescastaneda@gmail.com>
2023-10-18 04:32:09 -0500

    Se Agrega Sensor Podometro

--- PontiLiveApp/app/src/main/java/com/example/pontiliveapp/activities/RouteActivity.kt
+++ PontiLiveApp/app/src/main/java/com/example/pontiliveapp/activities/RouteActivity.kt
@@ -1,15 +1,22 @@
 package com.example.pontiliveapp.activities
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.os.StrictMode
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pontiliveapp.databinding.ActivityRouteBinding
 import com.example.pontiliveapp.model.Lugar
 import com.example.pontiliveapp.model.getLugares
@@ -28,37 +35,68 @@ import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Polyline
 
-
-class RouteActivity : AppCompatActivity() {
+class RouteActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding: ActivityRouteBinding
-    lateinit var map : MapView // Mapa
+    private lateinit var map: MapView // Mapa
     private lateinit var roadManager: RoadManager
     private var roadOverlay: Polyline? = null
-    lateinit var locationClient: FusedLocationProviderClient // Cliente de ubicación
-    lateinit var locationRequest : LocationRequest // Solicitud de ubicación
-    lateinit var locationCallback: LocationCallback // Callback de ubicación
-    lateinit var lastLocation : Location // Última ubicación conocida
-    lateinit var marker : Marker // Marcador
+    private lateinit var locationClient: FusedLocationProviderClient // Cliente de ubicación
+    private lateinit var locationRequest: LocationRequest // Solicitud de ubicación
+    private lateinit var locationCallback: LocationCallback // Callback de ubicación
+    private lateinit var lastLocation: Location // Última ubicación conocida
+    private lateinit var marker: Marker // Marcador
     private val permisosUbicacionRequestCode = 123 // Identificador único para la solicitud de permisos
-    private var destino : Lugar? = null
-    private var distancia : TextView? = null
-    private var tiempoRestante : TextView? = null
+    private var destino: Lugar? = null
+    private var distancia: TextView? = null
+    private var tiempoRestante: TextView? = null
+    private var pasos: TextView? = null
     private var bandera = true
-
-
+    private lateinit var sensorManager: SensorManager
+    private lateinit var stepSensor: Sensor
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRouteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.d("RouteActivity", "onCreate ejecutándose")
+
         distancia = binding.distancia
         tiempoRestante = binding.tiempoRestante
+        pasos = binding.pasos
 
-        binding.locationButton.setOnClickListener(){
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        for (sensor in sensorList) {
+            Log.d("RouteActivity", "Sensor disponible: ${sensor.name}")
+        }
+
+
+        try {
+            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+            if (stepSensor == null) {
+                // El dispositivo no tiene el sensor de contador de pasos
+                Toast.makeText(this,"Este dispositivo no admite el sensor de contador de pasos",Toast.LENGTH_SHORT).show()
+            } else {
+                // El sensor está disponible, puedes continuar con su uso
+                sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // Imprimir el mensaje de la excepción
+            Log.e("RouteActivity", "Error al obtener el sensor de contador de pasos: ${e.message}")
+            // Mostrar un mensaje al usuario
+            Toast.makeText(this, "Error al obtener el sensor de contador de pasos", Toast.LENGTH_SHORT).show()
+        }
+
+
+    binding.locationButton.setOnClickListener() {
             //centrar el mapa en la ubicacion del usuario
-            if(lastLocation!=null){
+            if (lastLocation != null) {
                 map.controller.setZoom(18.0)
                 val latitud = lastLocation.latitude
                 val longitud = lastLocation.longitude
@@ -67,7 +105,7 @@ class RouteActivity : AppCompatActivity() {
             }
         }
 
-        binding.cancelButton.setOnClickListener(){
+        binding.cancelButton.setOnClickListener() {
             finish()
         }
 
@@ -89,8 +127,6 @@ class RouteActivity : AppCompatActivity() {
 
         startLocationUpdates()
 
-
-
     }
 
     // metodo onPause
@@ -98,16 +134,35 @@ class RouteActivity : AppCompatActivity() {
         super.onPause()
         locationClient.removeLocationUpdates(locationCallback)
         map.onPause()
+        running = false
+        Log.d("RouteActivity", "onPause ejecutándose")
     }
 
     // metodo onResume
     override fun onResume() {
         super.onResume()
         map.onResume()
+        running = true
+        Log.d("RouteActivity", "onResume ejecutándose")
+    }
+
+    // Listener para el sensor de pasos
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Manejar cambios en la precisión si es necesario
+    }
+
+    // Listener para el sensor de pasos
+    override fun onSensorChanged(event: SensorEvent) {
+        if (running) {
+            totalSteps = event.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            binding.pasos.text = "Pasos: $currentSteps"
+            Log.d("RouteActivity", "Pasos actualizados: $currentSteps")
+        }
     }
 
     // funcion para establecer el mapa en la actividad
-    fun setMap(){
+    fun setMap() {
         Configuration.getInstance().load(this,
             androidx.preference.PreferenceManager.getDefaultSharedPreferences(this))
         map = binding.osmMap
@@ -115,16 +170,16 @@ class RouteActivity : AppCompatActivity() {
         map.setMultiTouchControls(true)
     }
 
-    fun drawRoute(start : GeoPoint, finish : GeoPoint){
+    fun drawRoute(start: GeoPoint, finish: GeoPoint) {
         var routePoints = ArrayList<GeoPoint>()
         routePoints.add(start)
         routePoints.add(finish)
         val road = roadManager.getRoad(routePoints)
         distancia!!.text = String.format("%.2f", road.mLength) + " km"
-        tiempoRestante!!.text = String.format("%.0f", road.mDuration/60) + " min"
+        tiempoRestante!!.text = String.format("%.0f", road.mDuration / 60) + " min"
 
-        if(map!=null){
-            if(roadOverlay != null){
+        if (map != null) {
+            if (roadOverlay != null) {
                 map.getOverlays().remove(roadOverlay);
             }
             roadOverlay = RoadManager.buildRoadOverlay(road)
@@ -134,7 +189,7 @@ class RouteActivity : AppCompatActivity() {
         }
     }
 
-    fun createLocationRequest() : LocationRequest{
+    fun createLocationRequest(): LocationRequest {
         locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
             .setWaitForAccurateLocation(true)
             .setMinUpdateIntervalMillis(3000)
@@ -142,16 +197,16 @@ class RouteActivity : AppCompatActivity() {
         return locationRequest
     }
 
-    fun createLocationCallBack() : LocationCallback{
-        val locationCallback = object : LocationCallback(){
+    fun createLocationCallBack(): LocationCallback {
+        val locationCallback = object : LocationCallback() {
             override fun onLocationResult(result: LocationResult) {
                 super.onLocationResult(result)
-                if(result!=null){
+                if (result != null) {
                     lastLocation = result.lastLocation!!
-                    if(map!=null){
-                        if(destino!=null){
+                    if (map != null) {
+                        if (destino != null) {
                             // limbia el mapa, crea los dos marcadores y centra el mapa en la ubicacion del usuario
-                            if(map.overlays.size>0){
+                            if (map.overlays.size > 0) {
                                 map.overlays.clear()
                             }
                             val start = GeoPoint(lastLocation.latitude, lastLocation.longitude)
@@ -167,7 +222,7 @@ class RouteActivity : AppCompatActivity() {
                             map.overlays.add(startMarker)
                             map.overlays.add(finishMarker)
                             drawRoute(start, finish)
-                            if(bandera){
+                            if (bandera) {
                                 map.controller.setZoom(18.0)
                                 val latitud = lastLocation.latitude
                                 val longitud = lastLocation.longitude
@@ -183,7 +238,7 @@ class RouteActivity : AppCompatActivity() {
         return locationCallback
     }
 
-    fun startLocationUpdates(){
+    fun startLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -193,7 +248,7 @@ class RouteActivity : AppCompatActivity() {
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             requestLocationPermission()
-        }else{
+        } else {
             locationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
         }
     }
@@ -201,7 +256,10 @@ class RouteActivity : AppCompatActivity() {
     private fun requestLocationPermission() {
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACTIVITY_RECOGNITION
+            ),
             permisosUbicacionRequestCode
         )
     }
